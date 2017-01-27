f231a03f9be6e8be70e3e260bdad8789c88567f9
SavinoSchiavone <savino.schiavone1@gmail.com>
2017-01-27 22:01:56 +0100

    Create example.php

---
+++ example.php
@@ -0,0 +1,21 @@
+<?php
+
+include("base.php);
+
+if($message == "/start"){
+  sendMessage($chat_id, "Hello! Welcome to the bot.");
+}
+
+//message with an inline keyboard
+if($message == "/inlinek"){
+  $menu1 = [
+    [
+      "text" => "Botton text",
+      "callback_data" => "callback_data"
+    ]
+   ];
+  $menu = ["inline_keyboard" => $menu1];
+  $menu = json_encode($menu);
+  sendMessage($chat_id, "Message with inline keyboard", $menu);
+  //I have to improve inline keyboard use, to do more easy
+}
