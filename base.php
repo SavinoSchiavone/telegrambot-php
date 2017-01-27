112e1ac70a9ea06642622da7d5253589178f7b51
SavinoSchiavone <savino.schiavone1@gmail.com>
2017-01-27 21:48:18 +0100

    Update base.php

--- base.php
+++ base.php
@@ -1,6 +1,6 @@
 <?php
 
-
+//replace TOKEN with your @botfather token
 define('telegram_api', "https://api.telegram.org/botTOKEN/");
 $input = file_get_contents('php://input');
 $update = json_decode($input, true);
@@ -14,8 +14,6 @@ $first_name = $update['message']['from']['first_name'];
 $last_name = $update['message']['from']['last_name'];
 $username = $update['message']['from']['username'];
 $chat_username = $update['message']['chat']['username'];
-$sex = ["M", "m", "F", "f"];
-$nationality = ["en", "it"];
 
 
 /*
@@ -25,17 +23,6 @@ $nationality = ["en", "it"];
 function sendMessage($chat_id, $message_text, $keyboard = null, $parse_mode = 'HTML')
 {
 	file_get_contents(telegram_api."sendMessage?chat_id=$chat_id&text=$message_text&parse_mode=$parse_mode&disable_web_page_preview=true&disable_notification=false&reply_to_message_id=false&reply_markup=$keyboard");
-}
-
-function generate_identity($sex, $nationality)
-{
-	if (in_array($nationality, $nation_list)) {
-		sendMessage($chat_id, "genero la identiti");
-	} else {
-		sendMessage($chat_id, "nazionalità non riconosciuta");
-		exit();
-	}
-	
 }
 
 
