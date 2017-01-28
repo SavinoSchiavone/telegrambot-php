4de65575bcfdb77843d59e5212c5620fd2f493ac
SavinoSchiavone <savino.schiavone1@gmail.com>
2017-01-28 16:55:27 +0100

    Full inline keyboard support

--- base.php
+++ base.php
@@ -1,28 +1,65 @@
 <?php
 
-//replace TOKEN with your @botfather token
+
 define('telegram_api', "https://api.telegram.org/botTOKEN/");
 $input = file_get_contents('php://input');
-$update = json_decode($input, true);
+$updates = json_decode($input, true);
 
 
 //main variables
-$message = $update['message']['text'];
-$user_id = $update['message']['from']['id'];
-$chat_id = $update['message']['chat']['id'];
-$first_name = $update['message']['from']['first_name'];
-$last_name = $update['message']['from']['last_name'];
-$username = $update['message']['from']['username'];
-$chat_username = $update['message']['chat']['username'];
+$message = $updates['message']['text'];
+$user_id = $updates['message']['from']['id'];
+$chat_id = $updates['message']['chat']['id'];
+$first_name = $updates['message']['from']['first_name'];
+$last_name = $updates['message']['from']['last_name'];
+$username = $updates['message']['from']['username'];
+$chat_username = $updates['message']['chat']['username'];
+//callback (inline keyboards) variables
+$cb_data = $updates['callback_query']['data'];
+$cb_id = $update["callback_query"]["id"];
+$cb_msg_id = $update["callback_query"]["message"]["message_id"];
+$cb_chat_id = $update["callback_query"]["message"]["chat"]["id"];
+$cb_user_id = $update["callback_query"]["from"]["id"];
+$cb_first_name = $update["callback_query"]["from"]["first_name"];
+$cb_last_name = $update["callback_query"]["from"]["last_name"];
+$cb_username = $update["callback_query"]["from"]["username"];
+
 
 
 /*
-*Use the function sendMessage to send a message
-*in the chat you want (see example on file bot.php)
+*Use the function sendMessage() to send a message
+*in the chat you want (see example on file example.php)
 */
 function sendMessage($chat_id, $message_text, $keyboard = null, $parse_mode = 'HTML')
 {
 	file_get_contents(telegram_api."sendMessage?chat_id=$chat_id&text=$message_text&parse_mode=$parse_mode&disable_web_page_preview=true&disable_notification=false&reply_to_message_id=false&reply_markup=$keyboard");
+}
+
+/*
+*Use the function cb_reply() to edit a message when
+*an inline button is pressed (see example on file example.php)
+*/
+function cb_reply($cb_id, $alert_text, $show_alert = false, $cb_msg_id = false, $new_message_text = false, $new_menu = false)
+{
+	global $cb_chat_id;
+
+	file_get_contents(telegram_api."answerCallbackQuery?callback_query_id=$cb_id&text=$alert_text&show_alert=$show_alert");
+
+	if($cb_msg_id)
+	{
+		if($new_menu)
+		{
+			
+			$rm = ['inline_keyboard' => $new_menu];
+			$reply_markup = json_encode($rm);
+		
+		}
+		if ($new_menu) {
+			file_get_contents(telegram_api."editMessageText?chat_id=$cb_chat_id&message_id=$cb_msg_id&text=$new_message_text&reply_markup=$reply_markup");
+		}else{
+			file_get_contents(telegram_api."editMessageText?chat_id=$cb_chat_id&message_id=$cb_msg_id&text=$new_message_text");
+		}
+	}
 }
 
 
