0e6aac92d4ea147ab6d5c5fd9a6b5179c8968b4e
SavinoSchiavone <savino.schiavone1@gmail.com>
2017-01-29 19:43:31 +0100

    Added the new function

--- example.php
+++ example.php
@@ -2,7 +2,7 @@
 
 include("base.php");
 
-if($message == "/start" or $cb_data == "/start"){
+if($message == "/start"){
   sendMessage($chat_id, "Hello! Welcome to the bot.");
 }
 
@@ -21,16 +21,16 @@ if($message == "/inlinek"){
   //I have to improve inline keyboard use, to do more easy
 }
 
-//change button and message text
 if($cb_data == "button1"){
-  $new_button = [
-    "text" => "Another button",
-    "callback_data" => "/start"
-   ];
+  $another_button = [
+      "text" => "Another awesome button",
+      "url" => "https://github.com/SavinoSchiavone/telegrambot-php"
+  ];
   $structure = [
-    [$new_button]
-    ];
+    [$another_button]
+  ];
   $menu = ["inline_keyboard" => $structure];
   $menu = json_encode($menu);
-  cb_reply($cb_id, "alert text", true, $cb_msg_id, "Another awesome button", $menu);
+  answerCallbackQuery($cb_id, "alert message", true);
+  editMessageText($cb_chat_id, $cb_msg_id, "Edited message", $menu);
 }
